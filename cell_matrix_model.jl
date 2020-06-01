3c1e93ccd9130d0208be8c61b9723815f73157bf
Isobel Taylor-Hearn <isobel.taylor-hearn@student.manchester.ac.uk>
2020-06-01 11:51:51 +0100

    Updated to use Delaunay triangulation to find the nearest neighbour of a given point. Includes a Morse Potential but no iteration through all cells

--- cell_matrix_model.jl
+++ cell_matrix_model.jl
@@ -1,10 +1,16 @@
+ENV["PLOTS_USE_ATOM_PLOTPANE"] = "false"
+using Plots
+using PyPlot
+using PyCall
+@pyimport matplotlib.pyplot as pyplt
+using VoronoiDelaunay
+scipy.spatial = pyimport("scipy.spatial")
+
 parameter_dict = Dict("centre_1_position_x" => 1, "centre_1_position_y" => 2,
                       "centre_2_position_x" => 3, "centre_2_position_y" => 4,
                       "current_radius" => 5, "current_orientation" => 6, "current_deformation" => 7 )
-
 maintained_parameters_dict = Dict("R_max" => 1, "dR_max" => 2, "dr_max" => 3,
                                   "dtheta_max" => 4, "dd_max" => 5, "initial_deformation_proportion" => 6)
-
 cluster_parameters_dict = Dict("matrix_stiffness" => 1, "attractor_start_x" => 2, "attractor_start_y" => 3,
                                 "attractor_end_x" => 4, "attractor_end_y" => 5, "attractor_strength" => 6)
 function create_maintained_parameters(R_max::Float64, dR_max::Float64, dr_max::Float64, dtheta_max::Float64, dd_max::Float64, initial_deformation_proportion::Float64)
@@ -35,6 +41,13 @@ function grow_i_cell(i_cell::Array{Float64, 1}, maintained_parameters::Array{Flo
         i_cell[parameter_dict["current_radius"]] = maintained_parameters[maintained_parameters_dict["R_max"]]
     end
     return i_cell
+end
+function draw_an_i_cell(i_cell::Array{Float64})
+    x_pos = i_cell[parameter_dict["centre_1_position_x"]]
+    y_pos = i_cell[parameter_dict["centre_1_position_y"]]
+    radius= i_cell[parameter_dict["current_radius"]]
+    circle = pyplt.Circle((x_pos, y_pos), radius, alpha=0.1 , color=:green, lw=0.3)
+    return circle
 end
 ###################FUNCTIONS FOR M CELLS#############
 function create_m_cell(centre_1_position_x::Float64, centre_1_position_y::Float64, centre_2_position_x::Float64, centre_2_position_y::Float64, current_radius::Float64, current_orientation::Float64, current_deformation::Float64)
@@ -67,11 +80,11 @@ function deform_m_cell(m_cell::Array{Float64, 1}, maintained_parameters::Array{F
             R-=0.001
 
             new_area = round(2*R*R * (pi - acos(d/(2*R))+ 0.5*d* sqrt(4*R*R-d*d)), digits=4)
-            print("decrease radius, r=", R," new area=", new_area, "max area=", max_area, "\n")
+            #print("decrease radius, r=", R," new area=", new_area, "max area=", max_area, "\n")
         else
             R+=0.001
             new_area = round(2*R*R * (pi - acos(d/(2*R))+ 0.5*d* sqrt(4*R*R-d*d)), digits=4)
-            print("increase radius, r=", R ," new area=", new_area,"max area=", max_area, "\n")
+            #print("increase radius, r=", R ," new area=", new_area,"max area=", max_area, "\n")
         end
     end
     m_cell[parameter_dict["current_radius"]] = round(R, digits=4)
@@ -91,6 +104,19 @@ function rotate_m_cell(m_cell::Array{Float64, 1}, maintained_parameters::Array{F
     m_cell[parameter_dict["centre_2_position_y"]] -= m_cell[parameter_dict["current_deformation"]]*0.5*sin(m_cell[parameter_dict["current_orientation"]])
     return m_cell
 end
+function draw_an_m_cell(m_cell::Array{Float64})
+    x_pos1 = m_cell[parameter_dict["centre_1_position_x"]]
+    y_pos1 = m_cell[parameter_dict["centre_1_position_y"]]
+    x_pos2 = m_cell[parameter_dict["centre_2_position_x"]]
+    y_pos2 = m_cell[parameter_dict["centre_2_position_y"]]
+    radius= m_cell[parameter_dict["current_radius"]]
+    circle1 = pyplt.Circle((x_pos1, y_pos1), radius, alpha = 0.1 , color = :blue, lw=0.3)
+    circle2 = pyplt.Circle((x_pos2, y_pos2), radius, alpha=0.1 , color = :blue, lw=0.3)
+    circles::Array{} = [circle1, circle2]
+    #pyplt.plot(draw_a_circle(x_pos1,y_pos1,radius_for_draw), seriestype = [:shape,], lw=1, aspect_ratio = 1, linecolor = :blue, fillalpha = 0)
+    #pyplt.plot(draw_a_circle(x_pos2,y_pos2,radius_for_draw), seriestype = [:shape,], lw=1, aspect_ratio = 1, linecolor = :blue, fillalpha = 0)
+    return circles
+end
 #############FUNCTIONS TO CHANGE CELL TYPE#######
 function replace_i_cell_with_m_cell(i_cell::Array{Float64, 1}, maintained_parameters::Array{Float64, 1})
     new_m_cell = create_m_cell(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
@@ -101,7 +127,7 @@ function replace_i_cell_with_m_cell(i_cell::Array{Float64, 1}, maintained_parame
 
     new_area::Float64 = round(2*R*R * (pi - acos(d/(2*R))+ 0.5*d* sqrt(4*R*R-d*d)), digits=3)
     while (abs(new_area-max_area))>0.5
-        if new_area - max_area >0.1
+        if new_area - max_area >0.5
             R-=0.01
             new_area = 2*R*R * (pi - acos(d/(2*R))+ 0.5*d* sqrt(4*R*R-d*d))
         else
@@ -137,6 +163,37 @@ function initialise_cluster(i_cell::Array{Float64,1})
     cluster::Array{Float64, 1} = i_cell
     return cluster
 end
+######FUNCTION TO FIND NEAREST NEIGHBOURS#####
+function find_neighbours(pindex, triang)
+        pindex -=1
+        neighbours::Array{Int64} = []
+        for i::Int64 in (1:1:size(triang.vertices,1))
+                for j::Int64 in (1:1:3)
+                        for k::Int64 in (1:1:3)
+                                if triang.vertices[i,j] == pindex
+                                        if j != k
+                                                neighbours=vcat(neighbours, triang.vertices[i,k])
+                                        end
+                                end
+                        end
+                end
+            end
+        for l::Int64 in (1:1:size(neighbours,1))
+                neighbours[l] += 1
+        end
+        print("The neighbours are", Set(neighbours), "\n\n")
+        return Set(neighbours)
+end
+function julia_delaunay_vertices(triang)
+    triang_vertices = triang.vertices
+    for i::Int64 in (1:1:size(triang_vertices,1))
+        for j::Int64 in (1:1:size(triang_vertices,2))
+            triang_vertices[i,j] += 1
+        end
+    end
+    return triang_vertices
+end
+
 function make_a_move(cluster::Array{Float64}, maintained_parameters::Array{Float64})
     index_of_cell_to_choose = rand(1: size(cluster,2)) # includes lower and upper number
     chosen_cell = cluster[:,index_of_cell_to_choose]
@@ -161,9 +218,9 @@ function make_a_move(cluster::Array{Float64}, maintained_parameters::Array{Float
 
     else
         #### M CELL PROTOCOL
-        if random_number <=2506/2506
+        if random_number <=250/2506
             chosen_cell=deform_m_cell(chosen_cell, maintained_parameters)
-        elseif random_number <= 1/2506
+        elseif random_number <= 500/2506
             chosen_cell=rotate_m_cell(chosen_cell, maintained_parameters)
         else
             chosen_cell=migrate_m_cell(chosen_cell, maintained_parameters)
@@ -183,67 +240,56 @@ function make_a_move(cluster::Array{Float64}, maintained_parameters::Array{Float
     cluster[:,index_of_cell_to_choose]=chosen_cell
     return cluster
 end
+function calculate_the_morse_potential(cluster::Array{Float64}, maintained_parameters::Array{Float64}, triang, De::Float64, a::Float64)
+    total_potential_contributed_by_cell = 0
+    index_of_first_cell = 1
+    first_cell = cluster[:,index_of_first_cell]
+    #neighbour_indices = find_neighbours(1,triang)
+    print(neighbour_indices)
+    if (first_cell[parameter_dict["centre_1_position_x"]] == first_cell[parameter_dict["centre_2_position_x"]]) && (
+                                    first_cell[parameter_dict["centre_1_position_y"]] == first_cell[parameter_dict["centre_2_position_y"]])
+        for i::Int16 in (1:1:size(cluster,2))
+            print("i=", i, "\n")
+            other_cell = cluster[:,i]
+            if (other_cell[parameter_dict["centre_1_position_x"]] == other_cell[parameter_dict["centre_2_position_x"]]) && (
+                                            other_cell[parameter_dict["centre_1_position_y"]] == other_cell[parameter_dict["centre_2_position_y"]])
+    #I cell protocol
+                equilibrium_radius::Float64 = first_cell[parameter_dict["current_radius"]] + other_cell[parameter_dict["current_radius"]]
+                dx::Float64 = first_cell[parameter_dict["centre_1_position_x"]] - other_cell[parameter_dict["centre_2_position_x"]]
+                dy::Float64 = first_cell[parameter_dict["centre_1_position_y"]] - other_cell[parameter_dict["centre_2_position_y"]]
+                r12::Float64 = sqrt((dx*dx)+(dy*dy))
 
-function draw_an_i_cell(i_cell::Array{Float64})
-    x_pos = i_cell[parameter_dict["centre_1_position_x"]]
-    y_pos = i_cell[parameter_dict["centre_1_position_y"]]
-    radius= i_cell[parameter_dict["current_radius"]]
-    circle = pyplt.Circle((x_pos, y_pos), radius, alpha=0.1 , color=:green, lw=0.3)
-    return circle
+                exp_factor::Float64 = exp(-a*(r12-equilibrium_radius))
+                potential::Float64 = De*(1-(exp_factor*exp_factor))*(1-(exp_factor*exp_factor))
+                total_potential_contributed_by_cell +=potential
+            end
+        end
+    end
+    return total_force_contributed_by_one_cell
 end
-    #pyplt.plot(draw_a_circle(x_pos,y_pos,radius_for_draw), seriestype = [:shape,], lw=1, aspect_ratio = 1, linecolor = :black, fillalpha = 0)
-function draw_an_m_cell(m_cell::Array{Float64})
-    x_pos1 = m_cell[parameter_dict["centre_1_position_x"]]
-    y_pos1 = m_cell[parameter_dict["centre_1_position_y"]]
-    x_pos2 = m_cell[parameter_dict["centre_2_position_x"]]
-    y_pos2 = m_cell[parameter_dict["centre_2_position_y"]]
-    radius= m_cell[parameter_dict["current_radius"]]
-    circle1 = pyplt.Circle((x_pos1, y_pos1), radius, alpha = 0.1 , color = :blue, lw=0.3)
-    circle2 = pyplt.Circle((x_pos2, y_pos2), radius, alpha=0.1 , color = :blue, lw=0.3)
-    circles::Array{} = [circle1, circle2]
-    #pyplt.plot(draw_a_circle(x_pos1,y_pos1,radius_for_draw), seriestype = [:shape,], lw=1, aspect_ratio = 1, linecolor = :blue, fillalpha = 0)
-    #pyplt.plot(draw_a_circle(x_pos2,y_pos2,radius_for_draw), seriestype = [:shape,], lw=1, aspect_ratio = 1, linecolor = :blue, fillalpha = 0)
-    return circles
-end
-
-
-ENV["PLOTS_USE_ATOM_PLOTPANE"] = "false"
-using Plots
-using PyPlot
-using PyCall
-@pyimport matplotlib.pyplot as pyplt
 
 
 function main()
     maintained_parameters = create_maintained_parameters(5.0, 0.05, 0.05, 0.000001, 0.05, 0.005)
-    print(maintained_parameters[maintained_parameters_dict["R_max"]])
     cluster_parameters = create_cluster_parameters(5.0,1.0,1.0,2.0,2.0,5.0)
     starting_cell = create_i_cell(1.0,1.0,4.9, 0.0)
     cluster = initialise_cluster(starting_cell)
-    print(cluster)
     i = 0
-    while i <500
-        #print("i=", i, "\n")
+    while i <5000
         cluster = make_a_move(cluster, maintained_parameters)
-        #print(cluster, "\n")
-
         i += 1
     end
     print(transpose(cluster))
     print("there are", size(cluster,2), "cells\n")
-
-    #plt = plot( bg = :white, xlim = (0.0, 20.0),  ylim = (-10.0, 10.0), framestyle = :none,
-    #size = (400, 400),  legend = false,)
-    #pyplt.figure(figsize = (5,5))
-    pyplt.clf()
+    plt.clf()
     ax=pyplt.gca()
     ax.set_xlim((-10, 10))
     ax.set_ylim((-10, 10))
     ax.set_aspect("equal")
+
     for i::Int16 in (1:1:size(cluster,2))
         print("i=", i, "\n")
         cell_to_draw = cluster[:,i]
-        #radius_for_draw = cell_to_draw[parameter_dict["current_radius"]]
         if (cell_to_draw[parameter_dict["centre_1_position_x"]] == cell_to_draw[parameter_dict["centre_2_position_x"]]) && (
                                         cell_to_draw[parameter_dict["centre_1_position_y"]] == cell_to_draw[parameter_dict["centre_2_position_y"]])
             #I CELL PROTOCOL
@@ -257,9 +303,29 @@ function main()
             #end
 
         end
-        fig = gcf()
-        display(fig)
+        #fig = gcf()
+        #display(fig)
     end
+
+    x_coords::Array{Float64} = vcat(cluster[1,:], cluster[3,:])
+    y_coords::Array{Float64}=vcat(cluster[2,:], cluster[4,:])
+
+    print("xcoords=", x_coords)
+    print("ycoord", y_coords)
+    points::Array{Float64}=(hcat(x_coords, y_coords))
+    dela=scipy.spatial.Delaunay
+    triang = dela(points)
+        #print(triang.points, "\n")
+    print(triang.vertices, "\n")
+    print(julia_delaunay_vertices(triang), "\n")
+    print(find_neighbours(2,triang))
+        #print(triang.neighbors[4])
+    plt.triplot(points[:,1], points[:,2], triang.simplices, color = :blue)
+    plt.plot(points[:,1], points[:,2], lw= 0, marker="o", linestyle="")
+    fig = gcf()
+    display(fig)
+    calculate_the_morse_force(cluster, maintained_parameters, triang, 1.0, 1.0)
+
 
 end
 
