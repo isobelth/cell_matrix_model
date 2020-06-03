e5fae01b646f7a0204face08bc071c41b08f60b8
Isobel Taylor-Hearn <isobel.taylor-hearn@student.manchester.ac.uk>
2020-06-03 09:55:32 +0100

    Working metropolis algorithm, some issues with cell deformation step

--- updated_cmm.jl
+++ updated_cmm.jl
@@ -69,25 +69,26 @@ function migrate_m_cell(m_cell::Array{Float64, 1}, maintained_parameters::Array{
 end
 function deform_m_cell(m_cell::Array{Float64, 1}, maintained_parameters::Array{Float64, 1})
     amount_to_deform = maintained_parameters[maintained_parameters_dict["dd_max"]]*rand()
-    m_cell[parameter_dict["current_deformation"]] += amount_to_deform
-    d::Float64=m_cell[parameter_dict["current_deformation"]]
-    R::Float64=m_cell[parameter_dict["current_radius"]]
-    x::Float64=d/(2*R)
+    d::Float64 =  m_cell[parameter_dict["current_deformation"]] + amount_to_deform
+    print("Deforming by", amount_to_deform, "new deformation =",  m_cell[parameter_dict["current_deformation"]], "\n")
+    R::Float64= m_cell[parameter_dict["current_radius"]]
+    #x::Float64=d/(2*R)
     max_area::Float64 = round(pi * maintained_parameters[maintained_parameters_dict["R_max"]] * maintained_parameters[maintained_parameters_dict["R_max"]], digits=4)
-    if x<=1
-        new_area::Float64 = round(2*R*R * (pi - acos(d/(2*R))+ 0.5*d* sqrt(4*R*R-d*d)), digits=3)
+    if (d/(2*R))<=1
+        new_area::Float64 = round(2*R*R * (pi - acos(d/(2*R))+ (0.5*d* sqrt(4*R*R-d*d))), digits=5)
     else
         new_area::Inf16
     end
     while (abs(new_area-max_area))>0.5
         if new_area - max_area >0.5
-            R-=0.001
-            new_area = round(2*R*R * (pi - acos(d/(2*R))+ 0.5*d* sqrt(4*R*R-d*d)), digits=4)
+            R-=0.01
+            new_area = round(2*R*R * (pi - acos(d/(2*R))+ (0.5*d* sqrt(4*R*R-d*d))), digits=5)
         else
-            R+=0.001
-            new_area = round(2*R*R * (pi - acos(d/(2*R))+ 0.5*d* sqrt(4*R*R-d*d)), digits=4)
+            R+=0.01
+            new_area = round(2*R*R * (pi - acos(d/(2*R))+ (0.5*d* sqrt(4*R*R-d*d))), digits=5)
         end
     end
+    m_cell[parameter_dict["current_deformation"]] = d
     m_cell[parameter_dict["current_radius"]] = round(R, digits=4)
     m_cell[parameter_dict["centre_1_position_x"]] += (amount_to_deform/2)*cos(m_cell[parameter_dict["current_orientation"]])
     m_cell[parameter_dict["centre_1_position_y"]] += (amount_to_deform/2)*sin(m_cell[parameter_dict["current_orientation"]])
@@ -115,14 +116,12 @@ function draw_an_m_cell(m_cell::Array{Float64})
     x_pos2 = m_cell[parameter_dict["centre_2_position_x"]]
     y_pos2 = m_cell[parameter_dict["centre_2_position_y"]]
     radius= m_cell[parameter_dict["current_radius"]]
+    print("The radius of the m cells to draw is", radius, "\n")
     circle1 = pyplt.Circle((x_pos1, y_pos1), radius, alpha = 0.1 , color = :blue, lw=0.3)
     circle2 = pyplt.Circle((x_pos2, y_pos2), radius, alpha=0.1 , color = :blue, lw=0.3)
     circles::Array{} = [circle1, circle2]
-    #pyplt.plot(draw_a_circle(x_pos1,y_pos1,radius_for_draw), seriestype = [:shape,], lw=1, aspect_ratio = 1, linecolor = :blue, fillalpha = 0)
-    #pyplt.plot(draw_a_circle(x_pos2,y_pos2,radius_for_draw), seriestype = [:shape,], lw=1, aspect_ratio = 1, linecolor = :blue, fillalpha = 0)
     return circles
 end
-#############FUNCTIONS TO CHANGE CELL TYPE#######
 function replace_i_cell_with_m_cell(i_cell::Array{Float64, 1}, maintained_parameters::Array{Float64, 1})
     new_m_cell = create_m_cell(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
     current_orientation = i_cell[parameter_dict["current_orientation"]]#####can update to divide in preferential directions
@@ -153,24 +152,21 @@ function replace_i_cell_with_m_cell(i_cell::Array{Float64, 1}, maintained_parame
     return new_m_cell
 end
 function replace_m_cell_with_two_i_cells(m_cell::Array{Float64, 1}, maintained_parameters::Array{Float64, 1})
-    R = m_cell[parameter_dict["current_radius"]]
+    r = m_cell[parameter_dict["current_radius"]]
     current_orientation = m_cell[parameter_dict["current_orientation"]]
     new_i_cell_1_x_position = m_cell[parameter_dict["centre_1_position_x"]]
     new_i_cell_1_y_position = m_cell[parameter_dict["centre_1_position_y"]]
     new_i_cell_2_x_position = m_cell[parameter_dict["centre_2_position_x"]]
     new_i_cell_2_y_position = m_cell[parameter_dict["centre_2_position_y"]]
-    new_i_cell_1::Array{Float64, 1} = create_i_cell(new_i_cell_1_x_position,new_i_cell_1_y_position,R,current_orientation)
-    new_i_cell_2::Array{Float64, 1} = create_i_cell(new_i_cell_2_x_position,new_i_cell_2_y_position,R,current_orientation)
+    new_i_cell_1::Array{Float64, 1} = create_i_cell(new_i_cell_1_x_position,new_i_cell_1_y_position,r,current_orientation)
+    new_i_cell_2::Array{Float64, 1} = create_i_cell(new_i_cell_2_x_position,new_i_cell_2_y_position,r,current_orientation)
     new_i_cells::Array{Float64} = hcat(new_i_cell_1 , new_i_cell_2)
-    #print("new i cells size", size(new_i_cells))
     return new_i_cells
 end
-############FUNCTION TO SET UP CLUSTER########
 function initialise_cluster(i_cell::Array{Float64,1})
     cluster::Array{Float64, 1} = i_cell
     return cluster
 end
-######FUNCTION TO FIND NEAREST NEIGHBOURS#####
 function find_neighbours(pindex, triang)
         pindex -=1
         neighbours::Array{Int64} = []
@@ -190,33 +186,24 @@ function find_neighbours(pindex, triang)
         return neighbours
 end
 function make_a_dictionary_of_neighbours(points::Array{Float64}, triang)
-        neighbours_dictionary = Dict()
-        for i::Int64 in (1:1:size(points,1))
+    neighbours_dictionary = Dict()
+    for i::Int64 in (1:1:size(points,1))
                 #key_for_dict = string(i)
-                key_for_dict = i
-                element_for_dict = find_neighbours(i,triang)
-                neighbours_dictionary[key_for_dict] = element_for_dict
-        end
-        return neighbours_dictionary
-end
-function julia_delaunay_vertices(triang)
-    triang_vertices = triang.vertices
-    for i::Int64 in (1:1:size(triang_vertices,1))
-        for j::Int64 in (1:1:size(triang_vertices,2))
-            triang_vertices[i,j] += 1
-        end
+            key_for_dict = i
+            element_for_dict = find_neighbours(i,triang)
+            neighbours_dictionary[key_for_dict] = element_for_dict
     end
-    return triang_vertices
+    return neighbours_dictionary
 end
-function make_a_move(cluster::Array{Float64}, maintained_parameters::Array{Float64})
+function make_a_move(cluster::Array{Float64}, maintained_parameters::Array{Float64}, number_of_cells::Int64)
     index_of_cell_to_choose = rand(1: size(cluster,2)) # includes lower and upper number
     chosen_cell = cluster[:,index_of_cell_to_choose]
     random_number = rand(Float64)
-    neighbour_dict = [0] #this is what will be returned if we have less than three cells
+    new_number_of_cells::Int64 = number_of_cells
     if (chosen_cell[parameter_dict["centre_1_position_x"]] == chosen_cell[parameter_dict["centre_2_position_x"]]) && (
                                     chosen_cell[parameter_dict["centre_1_position_y"]] == chosen_cell[parameter_dict["centre_2_position_y"]])
         #### I CELL PROTOCOL
-        if random_number <= 200/2501
+        if random_number <= 1/2501
             chosen_cell = grow_i_cell(chosen_cell, maintained_parameters)
         else
             chosen_cell = migrate_i_cell(chosen_cell, maintained_parameters)
@@ -225,36 +212,34 @@ function make_a_move(cluster::Array{Float64}, maintained_parameters::Array{Float
             print("replace I with M")
             chosen_cell = replace_i_cell_with_m_cell(chosen_cell, maintained_parameters)
         end
-        cluster[:,index_of_cell_to_choose]=chosen_cell
+        #cluster[:,index_of_cell_to_choose]=chosen_cell
     else
         #### M CELL PROTOCOL
-        if random_number <=250/2506
+        if random_number <=1/2506
+            print("cell index is", index_of_cell_to_choose, "\n")
             chosen_cell=deform_m_cell(chosen_cell, maintained_parameters)
-        elseif random_number <= 500/2506
+
+        elseif random_number <= 5/2506
             chosen_cell=rotate_m_cell(chosen_cell, maintained_parameters)
         else
             chosen_cell=migrate_m_cell(chosen_cell, maintained_parameters)
         end
-        cluster[:,index_of_cell_to_choose]=chosen_cell
-        if chosen_cell[parameter_dict["current_radius"]] <= maintained_parameters[maintained_parameters_dict["R_max"]]/(sqrt(2))
+        Rmrt::Float64 = maintained_parameters[maintained_parameters_dict["R_max"]]/sqrt(2)
+        #print("current radius = ", chosen_cell[parameter_dict["current_radius"]], "Rmrt=", Rmrt, "\n")
+        if chosen_cell[parameter_dict["current_radius"]] <=Rmrt
+
             print("Replace M with 2I \n")
             two_i_cells = replace_m_cell_with_two_i_cells(chosen_cell, maintained_parameters)
             chosen_cell = two_i_cells[:,1]
             other_cell = two_i_cells[:,2]
+            #cluster[:,index_of_cell_to_choose]=chosen_cell
             cluster = hcat(cluster, other_cell)
-            cluster[:,index_of_cell_to_choose]=chosen_cell
-            #####this is the only place we increase the number of cells, so we need to re-perform the neighbours list
-            if size(cluster, 2) >= 3
-                x_coords::Array{Float64} = cluster[parameter_dict["CoM_position_x"], :] #only CoM coords
-                y_coords::Array{Float64} = cluster[parameter_dict["CoM_position_y"], :] #only CoM coords
-                points = hcat(x_coords, y_coords)
-                dela=scipy.spatial.Delaunay
-                triang = dela(points)
-                neighbour_dict = make_a_dictionary_of_neighbours(points, triang)
-            end
+            new_number_of_cells = number_of_cells + 1
         end
+        #cluster[:,index_of_cell_to_choose]=chosen_cell
     end
-    return cluster, neighbour_dict
+    cluster[:,index_of_cell_to_choose]=chosen_cell
+    return cluster, new_number_of_cells
 end
 function morse_potential(cluster::Array{Float64}, neighbour_dict::Dict, De::Float64, a::Float64)
     total_potential::Float64 = 0
@@ -272,39 +257,63 @@ function morse_potential(cluster::Array{Float64}, neighbour_dict::Dict, De::Floa
 
                 exp_factor::Float64 = exp(-a*(r12-equilibrium_radius))
                 potential_for_pair::Float64 = De*(1-(exp_factor*exp_factor))*(1-(exp_factor*exp_factor))
-                print("i=", i, "j=", j, "potential for pair=", potential_for_pair, "\n")
+                #print("i=", i, "j=", j, "potential for pair=", potential_for_pair, "\n")
                 total_potential_contributed_by_cell += potential_for_pair
-                print("running total for cell ", i, "=", total_potential_contributed_by_cell, "\n")
+                #print("running total for cell ", i, "=", total_potential_contributed_by_cell, "\n")
             end
         end
-        print("i=", i, "total potential from cell =", total_potential_contributed_by_cell, "\n")
+        #print("i=", i, "total potential from cell =", total_potential_contributed_by_cell, "\n")
         total_potential += total_potential_contributed_by_cell
     end
-    print("total potential from cluster =", total_potential, "\n")
+    #print("total potential from cluster =", total_potential, "\n")
     return total_potential
 end
 function main()
+    x_coords::Array{Float64} = [0.0]
+    y_coords::Array{Float64} = [0.0]
+    points::Array = [0]
+    dela=sp.Delaunay
     maintained_parameters = create_maintained_parameters(5.0, 0.05, 0.05, 0.000001, 0.05, 0.005)
     cluster_parameters = create_cluster_parameters(5.0,1.0,1.0,2.0,2.0,5.0)
     starting_cell = create_i_cell(1.0,1.0,4.9, 0.0)
     cluster = initialise_cluster(starting_cell)
+    neighbour_dict = Dict
+    while size(cluster, 2) < 3
+        number_of_cells = size(cluster, 2)
+        cluster, new_number_of_cells = make_a_move(cluster, maintained_parameters, number_of_cells)
+        if number_of_cells == 2 && new_number_of_cells == 3
+            x_coords = cluster[parameter_dict["CoM_position_x"], :] #only CoM coords
+            y_coords = cluster[parameter_dict["CoM_position_y"], :] #only CoM coords
+            points = hcat(x_coords, y_coords)
+            triang = dela(points)
+            neighbour_dict = make_a_dictionary_of_neighbours(points, triang)
+            print("The neighbour dict is", neighbour_dict, "\n")
+        end
+    end
+
+    energy_before_making_change::Float64 = morse_potential(cluster, neighbour_dict, 5.0, 1.0)
     i::Int64 = 0
     count_accept_new::Int64 = 0
     count_keep_old::Int64 = 0
-    while size(cluster, 2) < 3
-        cluster, neighbour_dict = make_a_move(cluster, maintained_parameters)
-    end
-    cluster, neighbour_dict = make_a_move(cluster, maintained_parameters)
-    if size(cluster, 2) == 3
-        energy_before_making_change = morse_potential(cluster, neighbour_dict, 1.0, 1.0)
-    end
-    while size(cluster, 2) >= 3 &&  size(cluster, 2) <= 5
-        cluster, neighbour_dict = make_a_move(cluster, maintained_parameters)
-        cluster_after_making_change = make_a_move(cluster)
-        energy_after_making_change = morse_potential(cluster_after_making_change)
-        change_in_energy = energy_after_making_change - energy_before_making_change
+    #while size(cluster, 2) >= 3 && size(cluster, 2) < 4
+    while i < 1500
+        number_of_cells = size(cluster, 2)
+        cluster_after_making_change, new_number_of_cells = make_a_move(cluster, maintained_parameters, number_of_cells)
+        print("There were", number_of_cells, "cells and now there are", new_number_of_cells, "cells\n")
+        if number_of_cells != new_number_of_cells
+            x_coords = cluster_after_making_change[parameter_dict["CoM_position_x"], :] #only CoM coords
+            y_coords = cluster_after_making_change[parameter_dict["CoM_position_y"], :] #only CoM coords
+            points = hcat(x_coords, y_coords)
+            #dela = scipy.spatial.Delaunay
+            triang = dela(points)
+            neighbour_dict = make_a_dictionary_of_neighbours(points, triang)
+            print("The neighbour dict is", neighbour_dict, "\n")
+        end
+        energy_after_making_change::Float64 = morse_potential(cluster_after_making_change, neighbour_dict, 1.0, 1.0)
+        change_in_energy::Float64 = energy_after_making_change - energy_before_making_change
+        x::Float64 = change_in_energy/ (0.0001)
         random_number = rand()
-        exp_delta_E = 1 - change_in_energy + (0.5*change_in_energy*change_in_energy)
+        exp_delta_E =  (1 - (x) + (0.5*x*x))
         print("i=", i, "change in energy =", change_in_energy, "random number = ", random_number, "exp-deltaE=", exp_delta_E, "\n")
 
         if random_number <= exp_delta_E #change in energy < 0 if new state is lower energy
@@ -318,17 +327,16 @@ function main()
         i += 1
     end
     print("keep new =", count_accept_new, "keep old", count_keep_old, "\n")
-    print(transpose(cluster))
-    print("there are", size(cluster,2), "cells\n")
+
+
     plt.clf()
     ax=pyplt.gca()
-    ax.set_xlim((-10, 10))
-    ax.set_ylim((-10, 10))
+    ax.set_xlim((-30, 30))
+    ax.set_ylim((-30, 30))
     ax.set_aspect("equal")
 
-    for i::Int16 in (1:1:size(cluster,2))
-        #print("i=", i, "\n")
-        cell_to_draw = cluster[:,i]
+    for k::Int64 in (1:1:size(cluster,2))
+        cell_to_draw = cluster[:,k]
         if (cell_to_draw[parameter_dict["centre_1_position_x"]] == cell_to_draw[parameter_dict["centre_2_position_x"]]) && (
                                         cell_to_draw[parameter_dict["centre_1_position_y"]] == cell_to_draw[parameter_dict["centre_2_position_y"]])
             #I CELL PROTOCOL
@@ -339,21 +347,15 @@ function main()
             ax.add_artist(current_cell)
             current_cell = circles[2]
             ax.add_artist(current_cell)
-            #end
-
         end
     end
-    # x_coords::Array{Float64} = cluster[parameter_dict["CoM_position_x"], :] #only CoM coords
-    # y_coords::Array{Float64} = cluster[parameter_dict["CoM_position_y"], :] #only CoM coords
-    # points = hcat(x_coords, y_coords)
-    # dela=scipy.spatial.Delaunay
-    # triang = dela(points)
-    # plt.triplot(points[:,1], points[:,2], triang.simplices, color = :blue)
-    # plt.plot(points[:,1], points[:,2], lw= 0, marker="o", linestyle="")
-    # fig = gcf()
-    # display(fig)
-    #
-    # neighbour_dict = make_a_dictionary_of_neighbours(points, triang)
-    # print("morse pot is", morse_potential(cluster, neighbour_dict, 1.0, 1.0))
+    triang = dela(points)
+    plt.triplot(points[:,1], points[:,2], triang.simplices, color = :blue)
+    plt.plot(points[:,1], points[:,2], lw= 0, marker="o", linestyle="")
+    fig = gcf()
+    display(fig)
+
+    print(transpose(cluster))
 end
+
 main()
